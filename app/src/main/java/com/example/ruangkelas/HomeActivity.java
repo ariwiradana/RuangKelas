c0bd87a51fdf78097105e860638f57ad187b9603
ariwiradana <ariwiradana5@gmail.com>
2019-05-06 13:11:39 +0800

    Update ganti ID

--- app/src/main/java/com/example/ruangkelas/HomeActivity.java
+++ app/src/main/java/com/example/ruangkelas/HomeActivity.java
@@ -24,8 +24,8 @@ public class HomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addClass = findViewById(R.id.addClass);
+        addClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
